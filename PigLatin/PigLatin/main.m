674438249912b5566cb39a4288f33b29bde768c0
Cayo M Amorim <cayomesquita@gmail.com>
2020-06-11 00:35:06 -0700

    Complete Lab13

--- PigLatin/PigLatin/main.m
+++ PigLatin/PigLatin/main.m
@@ -7,11 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSString+PigLatin.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"%@", [@"Kale Chips" stringByPigLatinization]);
+        NSLog(@"%@", [@"kale chips" stringByPigLatinization]);
+        NSLog(@"%@", [@"Ale hips" stringByPigLatinization]);
     }
     return 0;
 }
