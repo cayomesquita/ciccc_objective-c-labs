e3005b19be49e1c95e18298a56ba7ad7f5236c2d
Cayo M Amorim <cayomesquita@gmail.com>
2020-06-14 18:24:46 -0700

    Initialize Lab10

---
+++ Payments/Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Cornerstone on 2020-06-14.
+//  Copyright © 2020 Cornerstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
